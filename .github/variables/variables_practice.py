30d331bc10aa93a88d844141eedb9246d8ee8e11
lamarc <91494758+Chochaser@users.noreply.github.com>
2021-10-28 14:10:38 -0400

    Update variables_practice.py

--- .github/variables/variables_practice.py
+++ .github/variables/variables_practice.py
@@ -3,3 +3,9 @@
 # Common Data Types and Variables in Python
 
 life_count = 10 #Integer value, all whole numbers, +/- and 0.
+account_balance = 7775.68 # Floating point value, +/-.
+user_name = "tommy crack hill" # String, a collection of letters/numbers.
+
+print(life_count)
+print(account_balance)
+print(user_name)
